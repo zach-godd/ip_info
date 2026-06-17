4184b20f2857118e0e848a2fc009667f2c511837
Zachary Goddard <zath0492@gmail.com>
2026-06-16 21:14:59 -0500

    add dmarc get

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -29,6 +30,35 @@ func (t *TestSuite) TestExtractDomain() {
 			domain, err := extractDomain(test.url)
 			t.Require().NoError(err)
 			t.Assert().Equal(test.expectedDomain, domain)
+		})
+	}
+}
+
+func (t *TestSuite) TestGetDMARC() {
+	tests := map[string]struct {
+		domain         string
+		expectedResult string
+		err            error
+	}{
+		"valid": {
+			domain:         "google.com",
+			expectedResult: "v=DMARC1; p=reject; rua=mailto:mailauth-reports@google.com",
+		},
+		"invalid domain": {
+			domain: "thisdomaintotallydoesnotexist.com",
+			err:    errors.New("lookup _dmarc.thisdomaintotallydoesnotexist.com: no such host"),
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func() {
+			result, err := getDMARCData(test.domain)
+			if test.err != nil {
+				t.Require().EqualError(err, test.err.Error())
+			} else {
+				t.Assert().NoError(err)
+			}
+
+			t.Assert().Equal(test.expectedResult, result)
 		})
 	}
 }
